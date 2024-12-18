ce39c59a2db87c75cd509866f06399b8a4630812
Saqib-nawab <saqibnawab823@gmail.com>
2024-12-18 09:38:30 +0500

    setup environment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Welcome to My Tailwind React App!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
